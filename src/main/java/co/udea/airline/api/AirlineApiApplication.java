0f33dea3d247365a65b3854f8e6e7fe5935460ed
JoseGomez14 <jose.gomez14@udea.edu.co>
2024-04-15 17:31:27 -0500

    fix: :ambulance: Add ModelMapper bean to AirlineApiApplication
    Co-authored-by: daniel-lujan <daniel.lujan@udea.edu.co>
    Co-authored-by: anavegaa <ana.vega@udea.edu.co>

--- src/main/java/co/udea/airline/api/AirlineApiApplication.java
+++ src/main/java/co/udea/airline/api/AirlineApiApplication.java
@@ -1,8 +1,10 @@
 package co.udea.airline.api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -16,7 +18,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 })
 
 public class AirlineApiApplication extends SpringBootServletInitializer {
-
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
     public static void main(String[] args) {
         SpringApplication.run(AirlineApiApplication.class, args);
     }
