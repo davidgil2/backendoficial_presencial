8faa5e6ff8fc9ebe8449cb8e2479c04840d8b122
daniel-lujan <daniel.lujan@udea.edu.co>
2024-04-17 20:25:44 -0500

    fix: :bug: Remove unnecessary fields from scale DTO body example
    Co-authored-by: JoseGomez14 <jose.gomez14@udea.edu.co>
    Co-authored-by: anavegaa <ana.vega@udea.edu.co>

--- src/main/java/co/udea/airline/api/model/DTO/flights/FlightDTO.java
+++ src/main/java/co/udea/airline/api/model/DTO/flights/FlightDTO.java
@@ -21,19 +21,19 @@ public class FlightDTO {
     @Max(value = 1000000, message = "The base price must be less than 1000000.")
     private double basePrice;
 
-    //@NotEmpty(message = "The flight must have a departure date.")
+    // @NotEmpty(message = "The flight must have a departure date.")
     @Schema(description = "The tax percent of the flight", required = true, example = "19.0")
     @Min(value = 0, message = "The tax percent must be greater than 0.")
     @Max(value = 100, message = "The tax percent must be less than 100.")
     private double taxPercent;
 
-    //@NotEmpty(message = "The flight must have a departure date.")
+    // @NotEmpty(message = "The flight must have a departure date.")
     @Schema(description = "The surcharge of the flight", required = true, example = "10.5")
     @Max(value = 100)
     @Min(value = 0, message = "The surcharge must be greater than 0.")
     private double surcharge;
 
-    @Schema(description = "The scales of the flight, the first scale is the origin and the last scale is the destination", required = true, example = "[{ \"id\": \"1\", \"airplaneModel\": { \"id\": \"A320-200\", \"family\": \"Boeing 737\", \"capacity\": 150, \"cargoCapacity\": 1500 }, \"originAirport\": { \"id\": \"BOG\", \"name\": \"El Dorado\", \"type\": \"International\", \"city\": \"Bogotá\", \"country\": \"Colombia\", \"runways\": 3 }, \"destinationAirport\": { \"id\": \"MDE\", \"name\": \"José María Córdova\", \"type\": \"International\", \"city\": \"Medellín\", \"country\": \"Colombia\", \"runways\": 2 }, \"departureDate\": \"2024-12-30 23:59:59\", \"arrivalDate\": \"2024-12-31 23:59:59\" }]")
+    @Schema(description = "The scales of the flight, the first scale is the origin and the last scale is the destination", required = true, example = "[{ \"airplaneModel\": { \"id\": \"A320-200\" }, \"originAirport\": { \"id\": \"BOG\" }, \"destinationAirport\": { \"id\": \"MDE\" }, \"departureDate\": \"2024-12-30 23:59:59\", \"arrivalDate\": \"2024-12-31 23:59:59\" }]")
     @Size(min = 1, max = 10, message = "The scales must be between 1 and 10 scales.")
     private ScaleDTO[] scales;
 }
