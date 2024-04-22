069945d322c4463849ce61c4d6da59e9d54d2e6b
MaritzaTC <maritza.tabarezc@udea.edu.co>
2024-04-21 23:33:04 -0500

    Add query

--- src/main/java/co/udea/airline/api/model/jpa/repository/flightbrepository/IScaleRespository.java
+++ src/main/java/co/udea/airline/api/model/jpa/repository/flightbrepository/IScaleRespository.java
@@ -1,12 +1,18 @@
 package co.udea.airline.api.model.jpa.repository.flightbrepository;
 
 import co.udea.airline.api.model.jpa.model.flightbmodel.Scale;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IScaleRespository extends JpaRepository<Scale, Long> {
 
-
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM scale WHERE flight_id = :flightId", nativeQuery = true)
+    void deleteScaleByFlightId(Long flightId);
 
 }
