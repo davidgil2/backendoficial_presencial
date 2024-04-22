cd5211c5827f77b9cd42f324d7c72e1a03545d71
AndresT2002 <andres.toloza@udea.edu.co>
2024-04-22 15:53:38 -0500

    Ajusté test general de la aplicación

--- src/test/java/com/udea/vuelo/VueloApplicationTests.java
+++ src/test/java/com/udea/vuelo/VueloApplicationTests.java
@@ -7,9 +7,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class VueloApplicationTests {
+
     @Test
     void contextLoads() {
-        assertThat(true).isTrue();
+        boolean result = true;
+        assertThat(result).isTrue();
     }
 
 }
